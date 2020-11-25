248903f44ee82565f1a85fa3aacf2ed14e768166
leand-santos <leandro.adriano428@gmail.com>
2020-11-25 16:27:58 -0300

    Init Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const { ApolloServer, gql } = require('apollo-server')
+const mongoose = require('mongoose')
+
+const typeDefs = require('./graphql/typeDefs')
+const resolvers = require('./graphql/resolvers')
+const { MONGODB } = require('./config')
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+mongoose.connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => server.listen({ port: 5000 }))
+    .then(res => console.log(`Server running a ${res.url}`))
